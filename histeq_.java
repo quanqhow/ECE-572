4da6ddcd16138f70332f6950a12cb6fbe070f9be
Steven Patrick <spatric5@vols.utk.edu>
2018-08-29 19:33:04 -0400

    Added Comments to HW2

--- histeq_.java
+++ histeq_.java
@@ -1,3 +1,8 @@
+// Name: Steven Patrick
+// Assignment: HW2 (ECE 572) 
+// Description: Equalize the histogram of an 8-bit image
+
+// Import Libraries
 import ij.*;
 import ij.process.*;
 import ij.gui.*;
@@ -7,30 +12,33 @@ import ij.plugin.filter.*;
 public class histeq_  implements PlugInFilter {
 	ImagePlus imp;
 
+	//Setup so it only works for 8-bit images
 	public int setup(String arg, ImagePlus imp) {
 		this.imp = imp;
 		return DOES_8G;
 	}
 
+	//Implementation
 	public void run(ImageProcessor ip) {
+		
+		// Get Image stats
 		int w = ip.getWidth();
 		int h = ip.getHeight();
-		int M = w*h;
 		int K = 256;
-
 		int[] H = ip.getHistogram();
-		//H[0] = (int)(Math.sqrt(H[0]));
-		for(int j = 0; j < H.length; j++){
-			H[j] = (int)(Math.sqrt(H[j]));
-		}
+		
+		// Sum of the square roots of all the previous bins
+		H[0] = (int)(Math.sqrt(H[0]));
 		for(int j = 1; j < H.length; j++){
-			H[j] = H[j]+H[j-1];
+			H[j] = (int)Math.sqrt(H[j])+H[j-1];
 		}
+
+		// Normalize lookup table
 		for(int j = 0; j < H.length; j++){
 			H[j] = (K-1)*H[j] / H[K-1];
 		}
+
+		//Apply lookup table
 		ip.applyTable(H);
-
 	}
-
 }
